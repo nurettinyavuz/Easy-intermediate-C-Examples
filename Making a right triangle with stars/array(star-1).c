52c8fcaee06510b715c745044f3c183759c83fe4
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-02-04 14:08:50 +0300

    Making a right triangle with stars
    Making a right triangle with stars

---
+++ Making a right triangle with stars/array(star-1).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*             
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+
+*/
+
+	int main() {
+		int i,j;
+	
+	for(i=1;i<9;i++){
+		
+	for(j=1;j<=i;j++){
+		  printf("*");        
+	  }
+	  printf("\n");
+		
+	}
+	return 0;
+	}
+
